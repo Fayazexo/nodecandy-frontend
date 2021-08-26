3097f171a305ae34bb4d7961dcf598fe31b4b71e
Fayaze H. Pantho <60003361+Fayazexo@users.noreply.github.com>
2021-08-26 10:45:50 +0600

    V2 Launch

--- src/components/GetStarted/Data.jsx
+++ src/components/GetStarted/Data.jsx
@@ -3,7 +3,7 @@ export const data = {
   topLine: "Don't get your ideas lost into space",
   headline: "Making softwares for your unique ideas",
   description:
-    "Whether it is a new business or a project, Every idea is an interesting one to us.",
+    "Whether it is a new business or a project, Every idea is an interesting one to us. Get us onboard with your project before it is late.",
   buttonLabel: "Get started",
   img: "../../images/Astronaut.svg",
   alt: "car",
